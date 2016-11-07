75e470cf571cb104f95d2ed812155f0ad85ba0d7
Jingbei Li <i@jingbei.li>
2016-11-07 21:58:38 +0800

    added tools to generate a stage file

---
+++ example-generator.py
@@ -0,0 +1,10 @@
+import json
+example='01-1'
+data={}
+data['width']=5
+data['height']=1
+data['hints']={}
+data['hints']['vertical']=['1 1 1']
+data['answer']=[]
+data['answer'].append([1,0,1,0,1])
+json.dump(data,open('examples/'+example+'.json','w'))
