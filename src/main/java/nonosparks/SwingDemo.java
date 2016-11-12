d27a58d25efb26011c319b837c93c3e1480c4be3
Jingbei Li <i@jingbei.li>
2016-11-12 22:28:41 +0800

    fix SwingDemo

--- src/main/java/nonosparks/SwingDemo.java
+++ src/main/java/nonosparks/SwingDemo.java
@@ -11,54 +11,56 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 
-class myMouseListener  implements MouseMotionListener
-{
-       public void mouseMoved(MouseEvent e){
-           int x=e.getX();
-           int y=e.getY();
-           String s="Mouse: "+x+','+y;
-           SwingDemo.jtf.setText(s);
-       }
-         public void mouseDragged(MouseEvent e){};
-}
 public class SwingDemo extends JFrame  {
-    private static final long serialVersionUID = 1L;
-    private static int size = 500;
-    public static JTextField jtf=new JTextField(30);
-    
-    public SwingDemo() {
-        super();
-        initUI();
-    }
+	private static final long serialVersionUID = 1L;
+	private static int size = 500;
 
-    private void initUI() {
-        JPanel contentPanel = new JPanel();
-        contentPanel.setLayout(new FlowLayout());
-        JButton button = new JButton("Double");
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                size = size * 2;
-                setSize(size, size);
-            }
-        });
-        jtf.setBounds(500, 750, 50, 20);
-        jtf.setHorizontalAlignment(JTextField.RIGHT);
-        contentPanel.add(button);
-        contentPanel.addMouseMotionListener(new myMouseListener());
-        getContentPane().add(contentPanel);
-        
-        // mainwindow
-        setTitle("Nonosparks");
-        setSize(size, size);
-        setResizable(false);
-        setLocation(700,300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-    }
+	public SwingDemo() {
+		super();
+		initUI();
+	}
 
-    public static void main(String[] args) {
-        SwingDemo mainwindow=new SwingDemo();
-        mainwindow.add(jtf);
-    }
+	private void initUI() {
+		setTitle("Nonosparks");
+		setSize(size, size);
+		JPanel contentPanel = new JPanel();
+		contentPanel.setLayout(null);
+		JButton button = new JButton("Double");
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				size = size * 2;
+				setSize(size, size);
+			}
+		});
+		button.setBounds(0,0,size,size/10);
+		contentPanel.add(button);
+		JTextField jtf=new JTextField(30);
+		jtf.setBounds(0, size/5*4, size, size/10);
+		jtf.setHorizontalAlignment(JTextField.RIGHT);
+		jtf.setEditable(false);
+		contentPanel.add(jtf);
+		class myMouseListener  implements MouseMotionListener {
+			public void mouseMoved(MouseEvent e) {
+				int x=e.getX();
+				int y=e.getY();
+				String s="Mouse: "+x+','+y;
+				jtf.setText(s);
+				button.setBounds(0,0,size,size/10);
+				jtf.setBounds(0, size/5*4, size, size/10);
+			}
+			public void mouseDragged(MouseEvent e) {};
+		}
+		contentPanel.addMouseMotionListener(new myMouseListener());
+		getContentPane().add(contentPanel);
+		// mainwindow
+		setResizable(false);
+		setLocation(700,300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		SwingDemo mainwindow=new SwingDemo();
+	}
 }
